fa5f03d960b18f5a37c28ce25113d8d67b347522
zwodek01 <zwodek01@gmail.com>
2019-07-09 19:20:42 +0200

    Add logic to component

--- src/app/components/color-component/color-component.component.ts
+++ src/app/components/color-component/color-component.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-color-component',
   templateUrl: './color-component.component.html',
   styleUrls: ['./color-component.component.scss']
 })
+
 export class ColorComponentComponent implements OnInit {
 
-  constructor() { }
+  @Output() onColorChoose = new EventEmitter<string>();
+
+  colors = ['blue', 'lightblue', 'green', 'orange', 'purple', 'pink']
+
+  constructor() {
+  }
 
   ngOnInit() {
+
+  }
+
+  chooseColor(color: string): void {
+    this.chosenColor = color;
+    this.onColorChoose.emit(color);
   }
 
 }
--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-start',
@@ -10,6 +11,24 @@ export class StartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  // private data: Observable<string>;
+  // private chosenColor: string;
+
+
+  value: string;
+
+
+  changed(value: string) {
+    console.log(`Child changed!`, value);
+    this.value = value;
+
+    // this.data = new Observable(observer => {
+    //   observer.next();
+    // });
+    // let subscription = this.data.subscribe(
+    //   value => this.chosenColor = color,
+    // );
   }
 
 }
