f16e8e8a8ae8aee7f9da9b4a4611406a071e07e3
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-09 11:32:46 +0200

    Add routing, initialize template components
    1. paths: '', 'start', 'register'
    2. start, register components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {StartComponent} from './components/start/start.component';
+import {RegisterComponent} from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'start', pathMatch: 'full'},
+  {path: 'start', component: StartComponent},
+  {path: 'register', component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './components/start/start.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
