66f7959ea2817eb5607e383cd3204d683bcbea9e
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-09 20:21:16 +0200

    Enable routing, make refactoring
    1. change name to start-background
    2. place start component to start component

---
+++ src/app/components/start-background/start-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-background',
+  templateUrl: './start-background.component.html',
+  styleUrls: ['./start-background.component.scss']
+})
+export class StartBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
