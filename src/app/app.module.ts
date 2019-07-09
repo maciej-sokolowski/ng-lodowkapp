7b1e24a0307b9a5beb8d017ad9071d4812687c5c
Marcin Kozicki <m.kozicki2@gmail.com>
2019-07-09 15:38:20 +0200

    Create  template for register form[WIP]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './components/start/start.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RegisterinputComponent } from './components/registerinput/registerinput.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegisterinputComponent
   ],
   imports: [
     BrowserModule,
