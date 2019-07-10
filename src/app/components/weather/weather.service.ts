6929e1e594af1a74a058d711b632e49b506f4ab4
Mateusz Zawadzki <zwodek01@gmail.com>
2019-07-10 23:54:42 +0200

    Fix URL Api

--- src/app/components/weather/weather.service.ts
+++ src/app/components/weather/weather.service.ts
@@ -18,7 +18,10 @@ export class WeatherService {
   }
 
   localWeather(lat: string, lon: string) {
-    return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=
-    de95355dadf51402b42727c557936c16&units=imperial`);
+    // return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=
+    // 886705b4c1182eb1c69f28eb8c520e20&units=imperial`);
+
+    return this.httpClient.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=886705b4c1182eb1c69f28eb8c520e20`);
+
   }
 }
