07605ad70dfa3f6dde1da88ee94c7ad165335111
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-11 14:33:47 +0200

    fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 // Modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
 // Components
-import { AppComponent } from './app.component';
-import { StartComponent } from './components/StartSection/start/start.component';
-import { RegisterComponent } from './components/RegisterSection/register/register.component';
-import { FridgeComponent } from './components/FridgeSection/fridge/fridge.component';
-import { ProductsComponent } from './components/FridgeSection/products/products.component';
-import { MainComponent } from './components/MainSection/main/main.component';
-import { VideoComponent } from './components/FridgeSection/video/video.component';
-import { ColorComponent } from './components/RegisterSection/color/color.component';
-import { RegistersummComponent } from './components/RegisterSection/registersumm/registersumm.component';
-import { AvatarComponent } from './components/avatar/avatar.component';
-import { WeatherComponent } from './components/MainSection/weather/weather.component';
-import { WeatherService } from './components/MainSection/weather/weather.service';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
-import { MembersContainer } from './components/StartSection/members-container/members-container.component';
-import { RegisterinputComponent } from './components/RegisterSection/registerinput/registerinput.component';
+import {AppComponent} from './app.component';
+import {StartComponent} from './components/StartSection/start/start.component';
+import {RegisterComponent} from './components/RegisterSection/register/register.component';
+import {FridgeComponent} from './components/FridgeSection/fridge/fridge.component';
+import {ProductsComponent} from './components/FridgeSection/products/products.component';
+import {MainComponent} from './components/MainSection/main/main.component';
+import {VideoComponent} from './components/FridgeSection/video/video.component';
+import {ColorComponent} from './components/RegisterSection/color/color.component';
+import {RegistersummComponent} from './components/RegisterSection/registersumm/registersumm.component';
+import {AvatarComponent} from './components/avatar/avatar.component';
+import {WeatherComponent} from './components/MainSection/weather/weather.component';
+import {WeatherService} from './components/MainSection/weather/weather.service';
+import {TopBarComponent} from './components/MainSection/top-bar/top-bar.component';
+import {MembersContainer} from './components/StartSection/members-container/members-container.component';
+import {RegisterinputComponent} from './components/RegisterSection/registerinput/registerinput.component';
 
 
 @NgModule({
@@ -28,8 +28,6 @@ import { RegisterinputComponent } from './components/RegisterSection/registerinp
     StartComponent,
     RegisterComponent,
     RegisterinputComponent,
-
-
     StartComponent,
     MembersContainer,
     FridgeComponent,
