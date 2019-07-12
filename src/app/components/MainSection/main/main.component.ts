18f4e4ba6f84bfadbf660aa477495d5cdbf2ee74
maciek-sokolowski <maciek.sokolowskii@gmail.com>
2019-07-12 15:51:39 +0200

    widget list

--- src/app/components/MainSection/main/main.component.ts
+++ src/app/components/MainSection/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { emit } from 'cluster';
 
 @Component({
   selector: 'app-main',
@@ -7,13 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  
+  isOpen: boolean;
+  emitIsSmallWidget: boolean;
+  emitLargeWidgetsList = ["Canvas", "Activities", "Products", "Notes"];
+  emitSmallWidgetsList =["Youtube - Tasty chanel", "Weather"];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  initList() {
+    this.isOpen = true;
+    
+
+    console.log(event.target.parentElement.getAttribute("id"));
+    if (event.target.parentElement.getAttribute("id") === "widget-4" || event.target.parentElement.getAttribute("id") === "widget-5") {
+      console.log("mały");
+      this.emitIsSmallWidget = true;
+    } else {
+      this.emitIsSmallWidget = false;
+    }
+  }
+
+  onClose(event) {
+    this.isOpen = false;
+    console.log(event)
+    if (event[1] === "small") {
+      this.emitSmallWidgetsList.splice(event[0], 1);
+    } else {
+      this.emitLargeWidgetsList.splice(event[0], 1);
+    }
+    console.log(this.emitLargeWidgetsList)
+    console.log(this.emitSmallWidgetsList)
+  }
 
 
 }
--- src/app/components/MainSection/widgets-list/widgets-list.component.ts
+++ src/app/components/MainSection/widgets-list/widgets-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Input} from '@angular/core';
 
 @Component({
   selector: 'app-widgets-list',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetsListComponent implements OnInit {
 
+  // largeWidgetsList = ["Canvas", "Activities", "Products", "Notes"]
+  // smallWidgetsList = ["Youtube - Tasty chanel", "Weather"]
+  
+  @Input() isSmallWidget: boolean;
+  @Input() largeWidgets: Array<string>;
+  @Input() smallWidgets: Array<string>;
+
+  @Output() emitClose = new EventEmitter();
+ 
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  chooseWidget() {
+    let currentWidget = event.target.innerText;
+    let widgetOnList: number;
+    let emiter: any;
+    if (this.isSmallWidget) {
+      widgetOnList = this.smallWidgets.indexOf(currentWidget);
+      emiter = [widgetOnList, "small"]
+    } else {
+      widgetOnList = this.largeWidgets.indexOf(currentWidget);
+      emiter = [widgetOnList, "large"]
+    }
+    this.emitClose.emit(emiter);
+    // console.log(this.largeWidgetsList);
+    // this.largeWidgetsList.splice(widgetOnList, 1);
+    // console.log(this.largeWidgetsList);
   }
 
 }
