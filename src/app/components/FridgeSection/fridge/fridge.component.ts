31bbfaeb63003dd09c238da8bc04cb2f7714f341
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-12 11:53:09 +0200

    Make switching between products and camera

--- src/app/components/FridgeSection/fridge/fridge.component.ts
+++ src/app/components/FridgeSection/fridge/fridge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-fridge',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FridgeComponent implements OnInit {
 
-  constructor() { }
+  isCameraVisible: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  setCameraVisible() {
+    this.isCameraVisible = true;
+  }
+
+  setListVisible() {
+    this.isCameraVisible = false;
   }
 
 }
