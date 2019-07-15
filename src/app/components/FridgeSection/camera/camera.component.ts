b4cde842b5b8dfef5858cda3a0d86259d18106c8
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-15 11:38:28 +0200

    Add mechanism of setting dots on fridge

--- src/app/components/FridgeSection/camera/camera.component.ts
+++ src/app/components/FridgeSection/camera/camera.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {Socket} from 'ngx-socket-io';
+import {Product} from '../../../interfaces/Models/product';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss']
 })
+
 export class CameraComponent implements OnInit {
 
   public imageUrl: string;
+  public dots = [];
 
   constructor(private socket: Socket) {
   }
@@ -16,6 +19,14 @@ export class CameraComponent implements OnInit {
   ngOnInit() {
     this.socket.on('image', (image) => this.imageUrl = `data:image/jpeg;base64,${image}`);
   }
+
+
+  setDot($event) {
+
+    const dot = {x: `${$event.layerX - 30}px`, y: `${$event.layerY - 30}px`};
+    this.dots.push(dot);
+  }
+
 
 }
 
