57fe09d3097c14e4be61eb85e211718ffc727f33
Marcin Kozicki <m.kozicki2@gmail.com>
2019-07-15 10:18:58 +0200

    Add little icon to recognize type of account

--- src/app/components/RegisterSection/registersumm/registersumm.component.ts
+++ src/app/components/RegisterSection/registersumm/registersumm.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-registersumm',
   templateUrl: './registersumm.component.html',
-  styleUrls: ['./../../avatar/avatar.component.scss', './../color/color.component.scss', './registersumm.component.scss']
+  styleUrls: ['./../../avatar/avatar.component.scss', './../color/color.component.scss', './../usertype/usertype.component.scss', './registersumm.component.scss']
 })
 export class RegistersummComponent implements OnInit {
+
+  userIcon: string;
 
   @Input()
   userName: string;
@@ -13,13 +15,29 @@ export class RegistersummComponent implements OnInit {
   chosenAvatar: string;
   @Input()
   chosenColor: string;
+  @Input()
+  userType: string;
+  @Input()
+  PIN?: string;
 
 
   constructor() { }
 
   ngOnInit() {
+    this.setUserTypeIcon();
     // this.userName = 'marcin';
     // this.chosenAvatar = 'cat';
     // this.chosenColor = 'pink';
+  }
+
+  setUserTypeIcon() {
+    console.log(this.userType);
+    if (this.userType === 'PARENT') {
+      this.userIcon = 'parent';
+      console.log(this.userIcon);
+    } else {
+      this.userIcon = 'child';
+    }
+
   }
 }
