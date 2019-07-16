a23c1b8959ee4f259f4907bffd932615408815dd
zwodek01 <zwodek01@gmail.com>
2019-07-16 08:06:39 +0200

    Add dumb component

--- src/app/components/MainSection/list-header/list-header.component.ts
+++ src/app/components/MainSection/list-header/list-header.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./list-header.component.scss']
 })
 export class ListHeaderComponent implements OnInit {
-  title = "Notes";
-  showList = "View all";
+  // title: string;
 
   @Input()
   canAddListItem: boolean = false;
+
+  @Input()
+  noteTitle: string = "";
+
+
 
   constructor() { }
 
