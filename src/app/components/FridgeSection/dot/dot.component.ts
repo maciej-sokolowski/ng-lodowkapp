5431789e8e8c8f1a5a5eed649317c77d79be5706
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-16 09:04:15 +0200

    Finish mechanism of excluding cloud labels

--- src/app/components/FridgeSection/dot/dot.component.ts
+++ src/app/components/FridgeSection/dot/dot.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, DoCheck, Input, OnInit} from '@angular/core';
 import {Product} from '../../../interfaces/Models/product';
 import {ProductService} from '../../../services/product.service';
 
@@ -14,28 +14,41 @@ enum Visible {
 })
 export class DotComponent implements OnInit {
 
+  static activeLabelID = '';
   @Input() product: Product;
-  visibleLabel = Visible.YES;
+  visibleLabel = Visible.NOT;
 
+  get ActiveLabelID() {
+    return DotComponent.activeLabelID;
+  }
+
+  set ActiveLabelID(value: string) {
+    DotComponent.activeLabelID = value;
+  }
 
   constructor(private prService: ProductService) {
 
   }
 
-  ngOnInit() {
 
+  ngOnInit() {
+    if (this.ActiveLabelID === '') {
+      this.ActiveLabelID = this.product.id;
+      this.visibleLabel = Visible.YES;
+    }
   }
 
-  changeLabelVisibility($event) {
-
-    console.log($event);
-    if (this.visibleLabel === Visible.YES) {
-      this.visibleLabel = Visible.NOT;
-    } else {
+  changeLabelVisibility() {
+    if (this.ActiveLabelID === '') {
+      this.ActiveLabelID = this.product.id;
       this.visibleLabel = Visible.YES;
+    } else {
+      if (this.ActiveLabelID === this.product.id) {
+        this.ActiveLabelID = '';
+        this.visibleLabel = Visible.NOT;
+      }
     }
 
   }
-
 
 }
