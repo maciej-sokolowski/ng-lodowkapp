f333a9874f4f30734704513d82ccd522b9187228
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-07-17 14:12:12 +0200

    Add validation messages under inputs

--- src/app/components/FridgeSection/edit/edit.component.ts
+++ src/app/components/FridgeSection/edit/edit.component.ts
@@ -1,7 +1,8 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {NameDate} from '../../../interfaces/name-date';
 import {ProductService} from '../../../services/product.service';
+import {validate} from 'codelyzer/walkerFactory/walkerFn';
 
 @Component({
   selector: 'app-edit',
@@ -16,20 +17,37 @@ export class EditComponent implements OnInit {
   }
 
 
-  nameDate = new FormGroup({
-    name: new FormControl(''),
-    expiry: new FormControl('')
+  public nameDate = new FormGroup({
+    name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.pattern('^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]*$')
+      ]
+    ),
+    expiry: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^([0-2][0-9]|(3)[0-1])(\\-)(((0)[0-9])|((1)[0-2]))(\\-)\\d{4}$')
+    ])
   });
+
+  get name() {
+    return this.nameDate.get('name');
+  }
+
+  get expiry() {
+    return this.nameDate.get('expiry');
+  }
 
   ngOnInit() {
   }
 
   emitDateAndName(formValue: FormGroup) {
-    console.log(formValue.value);
-    const data: NameDate = {...formValue.value};
-    console.log(data);
-    this.nameAndDateEmitter.emit(data);
-    this.nameDate.reset({});
+    if (formValue.valid) {
+      console.log(formValue.value);
+      const data: NameDate = {...formValue.value};
+      console.log(data);
+      this.nameAndDateEmitter.emit(data);
+      this.nameDate.reset({});
+    }
   }
-
 }
--- src/app/interfaces/Models/product.ts
+++ src/app/interfaces/Models/product.ts
@@ -3,7 +3,7 @@ import {Coordinate} from '../coordinate';
 export interface Product {
   id: string;
   name: string;
-  expiryDate: string;
+  expiryDate: Date;
   priority: number;
   needToBuy: boolean;
   fridgePosition: Coordinate;
